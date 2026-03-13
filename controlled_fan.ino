a81136fde47852ce3a1cd0c44bb07a2aa83f02e6
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-03-13 14:32:10 +0200

    temperature sensor added

--- controlled_fan.ino
+++ controlled_fan.ino
@@ -1,18 +1,23 @@
-int d1=5;
-int d2=6;
+int d1 = 5;
+int d2 = 6;
+int temp_sensor = A0;
 
 void setup() {
   pinMode(d1, OUTPUT);
   pinMode(d2, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  //Testing
+
+  int value = analogRead(temp_sensor);
+  Serial.println(value);
+
   digitalWrite(d1, HIGH);
   digitalWrite(d2, LOW);
   delay(1500);
+
   digitalWrite(d2, HIGH);
   digitalWrite(d1, LOW);
   delay(1500);
-
 }
