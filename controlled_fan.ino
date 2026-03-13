a02e4ab9aad4fb6374b48ca118a825a6c7235ed9
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-03-13 14:19:22 +0200

    added the motor + test

--- controlled_fan.ino
+++ controlled_fan.ino
@@ -1,11 +1,18 @@
-
+int d1=5;
+int d2=6;
 
 void setup() {
-  
-
+  pinMode(d1, OUTPUT);
+  pinMode(d2, OUTPUT);
 }
 
 void loop() {
-  
+  //Testing
+  digitalWrite(d1, HIGH);
+  digitalWrite(d2, LOW);
+  delay(1500);
+  digitalWrite(d2, HIGH);
+  digitalWrite(d1, LOW);
+  delay(1500);
 
 }
